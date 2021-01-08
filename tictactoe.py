265d4ee91ab44120e4bcbc46f6c871d7e166ad05
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-08 10:49:51 -0500

    Learned magic and dunder methods

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,3 @@
-result = input("please enter a value:")
 
 def user_choice():
 
@@ -11,7 +10,7 @@ def user_choice():
         if choice.isdigit() == False:
             print("Sorry that is not a digit!")
         if choice.isdigit():
-            if(int(choice in acceptable_range)):
+            if int(choice) in acceptable_range:
                 within_range = True
             else:
                 print('Sorry the number must be 0-10!')
@@ -20,4 +19,20 @@ def user_choice():
     return int(choice)
 
 
-user_choice()
+class Book():
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author 
+        self.pages = pages
+    def __str__(self):
+        return f'{self.title} by {self.author}'
+
+    def __len__(self):
+        return self.pages
+
+    def __del__(self):
+        print("DELETED")
+
+b = Book('Elon Musk', "Ashlee Vance", 350)
+
+print(len(b))
