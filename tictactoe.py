fdf0aa3c5f35bea097458dc16a5dd3e4e099dbfb
Ryan Flynn <ryanflynn44@gmail.com>
2021-01-08 11:18:49 -0500

    Added game board

--- tictactoe.py
+++ tictactoe.py
@@ -1,38 +1,31 @@
+game_board = [[" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
+            ]
 
+
+def display_game(game_board):
+    print('Here is the current board:')
+    for i in game_board:
+        print(i)
+    
 def user_choice():
 
     choice = 'WRONG'
-    acceptable_range = range(0,10)
+    acceptable_range = range(1,9)
     within_range = False
 
     while choice.isdigit() == False or within_range == False :
-        choice = input("Please enter a number (0-10): ")
+        choice = input("Please enter your position as a number (1-9): ")
         if choice.isdigit() == False:
-            print("Sorry that is not a digit!")
+            print("Sorry that is not a number!")
         if choice.isdigit():
             if int(choice) in acceptable_range:
                 within_range = True
             else:
-                print('Sorry the number must be 0-10!')
+                print('Sorry the number must be 1-9!')
                 within_range = False
 
     return int(choice)
 
-
-class Book():
-    def __init__(self, title, author, pages):
-        self.title = title
-        self.author = author 
-        self.pages = pages
-    def __str__(self):
-        return f'{self.title} by {self.author}'
-
-    def __len__(self):
-        return self.pages
-
-    def __del__(self):
-        print("DELETED")
-
-b = Book('Elon Musk', "Ashlee Vance", 350)
-
-print(len(b))
+display_game(game_board)
